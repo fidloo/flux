ccc258b8233cf8a1fe2d04e1016c6e8557d2c601
Kamran Gurbanli <k.qurbanli@gmail.com>
2023-05-11 23:49:31 +0300

    Fix package name

--- app/src/main/java/com/fidloo/flux/app/FluxApplication.kt
+++ app/src/main/java/com/fidloo/flux/app/FluxApplication.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.fidloo.flux.app.ui
+package com.fidloo.flux.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
